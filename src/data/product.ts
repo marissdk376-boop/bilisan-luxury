1cda3136ee417ec1c1408108e022dc9e62bab41f
marysTig <tigrine.marys06@gmail.com>
2026-07-31 00:26:21 +0100

    feat: add product data configuration file

--- src/data/product.ts
+++ src/data/product.ts
@@ -29,7 +29,7 @@ export const productData = {
     "/WhatsApp Image 2026-07-30 at 14.16.08.jpeg",
     "/WhatsApp Image 2026-07-30 at 14.16.09.jpeg",
   ],
-  backgroundPhoto: "/téléchargement (50).jfif",
+  backgroundPhoto: "/WhatsApp Image 2026-07-30 at 14.16.08.jpeg",
 
   // Pricing & Packs
   defaultPackId: "1kg",
